c125db40fc92e9eea45d3572580b7926a29494d8
Alessio Sferro <sferro.alessio@gmail.com>
2020-03-03 06:25:16 +0100

    Update block script invocation

--- js/config/app.config.js
+++ js/config/app.config.js
@@ -1,5 +1,5 @@
 export const APP_DEFAULT_CONFIG = {
   PLAYABLE_BLOCKS: {
-    INTERACTION: false
+    interaction: false
   }
 };
--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 import { APP_DEFAULT_CONFIG } from './config/app.config.js';
 import interaction from './functions/interaction.js';
 
-const { INTERACTION } = APP_DEFAULT_CONFIG.PLAYABLE_BLOCKS;
+const BLOCKS = APP_DEFAULT_CONFIG.PLAYABLE_BLOCKS;
+
+// i nomi dei blocchi di codice devono coincidere con le chiavi
+// del seguente oggetto, per chiamare correttamente il blocc
+// di codice associato.
+const FUNCTIONS = {
+  interaction
+};
 
 // seleziona la porzione di codice da eseguire
 // in base alla configurazione dell'app
-switch (true) {
-  case INTERACTION:
-    interaction();
-    break;
+for (const blockName in BLOCKS) {
+  if (BLOCKS[blockName]) FUNCTIONS[blockName]();
 }
